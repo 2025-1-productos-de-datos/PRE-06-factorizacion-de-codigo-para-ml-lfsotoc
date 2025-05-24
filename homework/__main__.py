59b40a2470a6499f4907117ffec1d87449942a22
lfsotoc <lfsotoc@unal.edu.co>
2025-05-24 08:00:19 -0500

    Intento

---
+++ homework/__main__.py
@@ -0,0 +1,9 @@
+# __main__.py
+"""Entry point for the homework package."""
+
+# python3 -m homework __model elasticnet --l1_ratio 0.1 --alpha 0.9
+
+from .src.main import main
+
+if __name__ == "__main__":
+    main()
